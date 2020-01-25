0dc834f4ba1d999ed05e3654146f9a40633ad3a0
Conor McCue <conormccue@gmail.com>
2020-01-24 21:59:29 -0800

    mem-data-model

--- models/memory-data-model.js
+++ models/memory-data-model.js
@@ -6,6 +6,7 @@ class Model {
   constructor(){
     this.database = [];
   }
+
   get(id){
     let response = id ? this.database.filter( record => record.id === id) : this.database;
     return Promise.resolve(response);
@@ -16,5 +17,17 @@ class Model {
     this.database.push(record);
     return Promise.resolve(record);
   }
+
+  update(id, record){
+    this.database = this.database.map(item => item.id === id ? record : item);
+    return Promise.resolve(record);
+  }
+
+  delete(id){
+    this.database = this.database.filter( record => record.id !== id);
+    return Promise.resolve();
+  }
 }
+
+module.exports = Model;
 
